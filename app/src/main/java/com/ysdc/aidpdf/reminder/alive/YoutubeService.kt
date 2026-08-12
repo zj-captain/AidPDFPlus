ddada16df397524d76529eb4b47c9f131ede3485
YangBin <99154650@qq.com>
2026-08-12 12:36:36 +0800

    add youtube

---
+++ app/src/main/java/com/ysdc/aidpdf/reminder/alive/YoutubeService.kt
@@ -0,0 +1,12 @@
+package com.ysdc.aidpdf.reminder.alive
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class YoutubeService : Service() {
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+}
